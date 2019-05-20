1b7edb1f95cbff2c8bde5764a305d3fd28568f8e
andy <andyarok@gmail.com>
2019-05-20 07:47:00 +0530

    Modified the build file to generate dist
    Modified the build file. Added ignore folders to ignore list.
    Modified package.json
    
    Using build.js instead of npm run, since npm run is failing.

--- main/build.js
+++ main/build.js
@@ -1,7 +1,8 @@
 var NwBuilder = require('nw-builder');
 var nw = new NwBuilder({
-    files: './src/**',	// use the glob format
-	platforms: ['linux64', 'win64']
+    files: '../build/**',	// use the glob format
+	  platforms: ['linux64', 'win64'],
+    buildDir: '../dist'
 });
 
 // .build() returns a promise but also supports a plain callback approach as well
